2948337a7ec6d86fdb35eafabc041b50c3b08e15
thefelipe001 <elfelipe_lugo@hotmail.com>
2024-10-29 16:28:48 -0400

    Consumir Servicio Externo

--- SkyForecastAPI/obj/Debug/net8.0/SkyForecastAPI.AssemblyInfo.cs
+++ SkyForecastAPI/obj/Debug/net8.0/SkyForecastAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyForecastAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0444cfc708820360af37115f44f31d11edbfceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyForecastAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyForecastAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
